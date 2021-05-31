83b10d3bd881e598149e3de99453f38477588ef4
xuyuxiang <xyxsuoer@126.com>
2021-05-31 23:30:43 +0800

    制作StatsComponent

--- Source/MirumoWorld/Private/Components/XYXStatsManagerComponent.cpp
+++ Source/MirumoWorld/Private/Components/XYXStatsManagerComponent.cpp
@@ -2,33 +2,259 @@
 
 
 #include "Components/XYXStatsManagerComponent.h"
+#include "Interfaces/XYXInterfaceItem.h"
+#include "Items/ObjectItems/XYXItemBase.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/XYXEquipmentManagerComponent.h"
 
 // Sets default values for this component's properties
 UXYXStatsManagerComponent::UXYXStatsManagerComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
 
-
-// Called when the game starts
-void UXYXStatsManagerComponent::BeginPlay()
+void UXYXStatsManagerComponent::ExcludeItemModifiers(TSubclassOf<class UXYXItemBase> Item)
 {
-	Super::BeginPlay();
-
-	// ...
-	
+	if (UKismetSystemLibrary::IsValidClass(Item))
+	{
+		UXYXItemBase* ItemBase = NewObject<UXYXItemBase>(this, Item);
+		if (ItemBase)
+		{
+			IXYXInterfaceItem* MyInterface = Cast<IXYXInterfaceItem>(ItemBase);
+			if (MyInterface)
+			{
+				auto&& TmpVec = MyInterface->GetModifiers();
+				for (auto && e : TmpVec)
+				{
+					RemoveModifier(e.Type, e.Value);
+				}
+			}
+		}
+	}
 }
 
-
-// Called every frame
-void UXYXStatsManagerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UXYXStatsManagerComponent::IncludeItemModifiers(TSubclassOf<class UXYXItemBase> Item)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (UKismetSystemLibrary::IsValidClass(Item))
+	{
+		UXYXItemBase* ItemBase = NewObject<UXYXItemBase>(this, Item);
+		if (ItemBase)
+		{
+			IXYXInterfaceItem* MyInterface = Cast<IXYXInterfaceItem>(ItemBase);
+			if (MyInterface)
+			{
+				auto&& TmpVec = MyInterface->GetModifiers();
+				for (auto&& e : TmpVec)
+				{
+					AddModifier(e.Type, e.Value);
+				}
+			}
+		}
+	}
+}
 
-	// ...
+void UXYXStatsManagerComponent::AddModifier(EAttributesType Type, float Value)
+{
+	if (Value == 0.f)
+	{
+		return;
+	}
+
+	int32 TmpIndex = GetStatIndex(Type);
+	if (TmpIndex >= 0)
+	{
+		if (Stats.IsValidIndex(TmpIndex))
+		{
+			Stats[TmpIndex].ModifiersValue += Value;
+			OnModifierAdded.Broadcast(Type, Value);
+		}
+	}
+
+}
+
+void UXYXStatsManagerComponent::RemoveModifier(EAttributesType Type, float Value)
+{
+	if (Value == 0.f)
+	{
+		return;
+	}
+
+	int32 TmpIndex = GetStatIndex(Type);
+	if (TmpIndex >= 0)
+	{
+		if (Stats.IsValidIndex(TmpIndex))
+		{
+			Stats[TmpIndex].ModifiersValue -= Value;
+			OnModifierRemoved.Broadcast(Type, Value);
+		}
+	}
+}
+
+float UXYXStatsManagerComponent::GetStatValue(EAttributesType Type, bool bIncludeModifiers)
+{
+	float Value = 0.f;
+	int32 TmpIndex = GetStatIndex(Type);
+	if (TmpIndex >= 0)
+	{
+		if (Stats.IsValidIndex(TmpIndex))
+		{
+			if (bIncludeModifiers)
+			{
+				Value = Stats[TmpIndex].BaseValue + Stats[TmpIndex].ModifiersValue;
+			}
+			else
+			{
+				Value = Stats[TmpIndex].BaseValue;
+			}
+		}
+	}
+
+	return Value;
+}
+
+int32 UXYXStatsManagerComponent::GetStatIndex(EAttributesType Type)
+{
+	int32 Index = -1;
+
+	for (int32 i = 0; i< Stats.Num(); ++i)
+	{
+		if (Stats[i].Type == Type)
+		{
+			Index = i;
+			break;
+		}
+	}
+
+	return Index;
+}
+
+float UXYXStatsManagerComponent::GetDamage()
+{
+	float RandomValue = UKismetMathLibrary::RandomFloatInRange(1.f, 100.f);
+	float TmpValue = 1.0f;
+	float Damage = 0.f;
+	if (RandomValue <= GetStatValue(EAttributesType::ECritChance, true))
+	{
+		TmpValue = GetStatValue(EAttributesType::ECritMultiplier, true);
+	}
+
+	Damage = GetStatValue(EAttributesType::EDamage, true) * TmpValue;
+	Damage = UKismetMathLibrary::FTrunc(Damage);
+
+	return Damage;
+
+}
+
+void UXYXStatsManagerComponent::ChangeStatBaseValue(EAttributesType Type, float Value)
+{
+	int32 TmpIndex = GetStatIndex(Type);
+	if (Stats.IsValidIndex(TmpIndex))
+	{
+		Stats[TmpIndex].BaseValue = Value;
+		OnBaseValueChanged.Broadcast(Type, Value);
+	}
+}
+
+void UXYXStatsManagerComponent::TakeDamage(float Damage, bool bIgnoreStamina)
+{
+	UWorld* World = GetWorld();
+	check(World);
+
+	World->GetTimerManager().ClearTimer(ResetRecentlyReceivedDamageTimer);
+	RecentlyReceivedDamage += Damage;
+	RecentlyReceivedHitsCount++;
+	World->GetTimerManager().SetTimer(ResetRecentlyReceivedDamageTimer, this, &UXYXStatsManagerComponent::ResetRecentlyReceivedDamage, 4.f, false);
+
+}
+
+float UXYXStatsManagerComponent::GetRecentlyReceivedDamage()
+{
+	return RecentlyReceivedDamage;
+}
+
+void UXYXStatsManagerComponent::ResetRecentlyReceivedDamage()
+{
+	RecentlyReceivedDamage = 0.f;
+	RecentlyReceivedHitsCount = 0;
+	RecentlyReceivedSuccessfulDamage = 0.f;
+	RecentlyReceivedSuccessfulHitsCount = 0;
+}
+
+int32 UXYXStatsManagerComponent::GetRecentlyReceivedHitsCount()
+{
+	return RecentlyReceivedHitsCount;
+}
+
+int32 UXYXStatsManagerComponent::GetRecentlyReceivedSuccessfulHitsCount()
+{
+	return RecentlyReceivedSuccessfulHitsCount;
+}
+
+float UXYXStatsManagerComponent::GetRecentlyReceivedSuccessfulDamage()
+{
+	return RecentlyReceivedSuccessfulDamage;
+}
+
+void UXYXStatsManagerComponent::UpdateBlockBaseValue()
+{
+	if (EquipmentComp)
+	{
+		float TmpValue = 0.f;
+		bool TmpSuccess = false;
+		EquipmentComp->GetBlockValue(TmpValue, TmpSuccess);
+		float NewValue = InitialBlockValue;
+		if (TmpSuccess)
+		{
+			NewValue = TmpValue;
+		}
+
+		ChangeStatBaseValue(EAttributesType::EBlock, NewValue);
+	}
+}
+
+void UXYXStatsManagerComponent::Initialize()
+{
+	InitialBlockValue = GetStatValue(EAttributesType::EBlock, false);
+	EquipmentComp = Cast<UXYXEquipmentManagerComponent>(GetOwner()->GetComponentByClass(UXYXEquipmentManagerComponent::StaticClass()));
+	if (EquipmentComp)
+	{
+		EquipmentComp->OnActiveItemChanged.AddDynamic(this, &UXYXStatsManagerComponent::HandleOnActiveItemChanged);
+		EquipmentComp->OnSlotHiddenChanged.AddDynamic(this, &UXYXStatsManagerComponent::HandleOnSlotHiddenChanged);
+		EquipmentComp->OnMainHandTypeChanged.AddDynamic(this, &UXYXStatsManagerComponent::HandleOnMainHandTypeChanged);
+	}
+
+}
+
+void UXYXStatsManagerComponent::HandleOnActiveItemChanged(FStoredItem OldItem, FStoredItem NewItem, EItemType Type, int32 SlotIndex, int32 ActiveIndex)
+{
+	UpdateBlockBaseValue();
+
+	if (EquipmentComp && !EquipmentComp->IsSlotHidden(Type, SlotIndex))
+	{
+		IncludeItemModifiers(NewItem.ItemClass);
+		ExcludeItemModifiers(OldItem.ItemClass);
+	}
+}
+
+void UXYXStatsManagerComponent::HandleOnSlotHiddenChanged(EItemType SlotType, int32 SlotIndex, FStoredItem ActiveItem, bool bIsHidden)
+{
+	if (bIsHidden)
+	{
+		ExcludeItemModifiers(ActiveItem.ItemClass);
+	}
+	else
+	{
+		IncludeItemModifiers(ActiveItem.ItemClass);
+	}
+}
+
+void UXYXStatsManagerComponent::HandleOnMainHandTypeChanged(EItemType Type)
+{
+	UpdateBlockBaseValue();
 }
 
--- Source/MirumoWorld/Public/Components/XYXStatsManagerComponent.h
+++ Source/MirumoWorld/Public/Components/XYXStatsManagerComponent.h
@@ -4,8 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Game/XYXData.h"
 #include "XYXStatsManagerComponent.generated.h"
 
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnModifierAdded, EAttributesType, Type, float, Value);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnModifierRemoved, EAttributesType, Type, float, Value);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnBaseValueChanged, EAttributesType, Type, float, NewValue);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MIRUMOWORLD_API UXYXStatsManagerComponent : public UActorComponent
@@ -16,13 +22,94 @@ public:
 	// Sets default values for this component's properties
 	UXYXStatsManagerComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+	UPROPERTY(BlueprintAssignable)
+		FOnModifierAdded OnModifierAdded;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(BlueprintAssignable)
+		FOnModifierRemoved OnModifierRemoved;
 
-		
+	UPROPERTY(BlueprintAssignable)
+		FOnBaseValueChanged OnBaseValueChanged;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = XYX)
+		class UXYXEquipmentManagerComponent* EquipmentComp;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		TArray<FAttributes> Stats;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		float InitialBlockValue = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		float RecentlyReceivedDamage = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		int32 RecentlyReceivedHitsCount = 0;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		float RecentlyReceivedSuccessfulDamage = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		int32 RecentlyReceivedSuccessfulHitsCount = 0;
+
+	UPROPERTY(BlueprintReadOnly, Category = XYX)
+		FTimerHandle ResetRecentlyReceivedDamageTimer;
+
+	UFUNCTION()
+		void ExcludeItemModifiers(TSubclassOf<class UXYXItemBase> Item);
+
+	UFUNCTION()
+		void IncludeItemModifiers(TSubclassOf<class UXYXItemBase> Item);
+
+	UFUNCTION()
+		void AddModifier(EAttributesType Type, float Value);
+
+	UFUNCTION()
+		void RemoveModifier(EAttributesType Type, float Value);
+
+	UFUNCTION()
+		float GetStatValue(EAttributesType Type, bool bIncludeModifiers);
+
+	UFUNCTION()
+		int32 GetStatIndex(EAttributesType Type);
+
+	UFUNCTION()
+		float GetDamage();
+
+	UFUNCTION()
+		void ChangeStatBaseValue(EAttributesType Type, float Value);
+
+	UFUNCTION()
+		void TakeDamage(float Damage, bool bIgnoreStamina);
+
+	UFUNCTION()
+		float GetRecentlyReceivedDamage();
+
+	UFUNCTION()
+		void ResetRecentlyReceivedDamage();
+
+	UFUNCTION()
+		int32 GetRecentlyReceivedHitsCount();
+
+	UFUNCTION()
+		int32 GetRecentlyReceivedSuccessfulHitsCount();
+
+	UFUNCTION()
+		float GetRecentlyReceivedSuccessfulDamage();
+
+	UFUNCTION()
+		void UpdateBlockBaseValue();
+
+	UFUNCTION()
+		void Initialize();
+
+	UFUNCTION()
+		void HandleOnActiveItemChanged(FStoredItem OldItem, FStoredItem NewItem, EItemType Type, int32 SlotIndex, int32 ActiveIndex);
+
+	UFUNCTION()
+		void HandleOnSlotHiddenChanged(EItemType SlotType, int32 SlotIndex, FStoredItem ActiveItem, bool bIsHidden);
+
+	UFUNCTION()
+		void HandleOnMainHandTypeChanged(EItemType Type);
+
 };
